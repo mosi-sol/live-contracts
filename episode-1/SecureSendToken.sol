9a519954bd46fe766d49455b8749415b3ffb7d0a
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-02-12 08:29:26 +0330

    add

---
+++ episode-1/SecureSendToken.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0  
+
+pragma solidity 0.8.0;
+
+// not secure method
+contract unsecureMethod {
+	mapping(address => uint) private userBalances;
+
+	function withdraw() public {
+		uint amountToWithdraw = userBalances[msg.sender];
+		(bool success, ) = msg.sender.call.value(amountToWithdraw)("");
+		require(success);
+		userBalances[msg.sender] = 0;
+	}
+}
+
+// pure secure method
+contract secureMethod {
+	mapping(address => uint) private userBalances;
+
+	function withdraw() public {
+		uint amountToWithdraw = userBalances[msg.sender];
+		userBalances[msg.sender] = 0;
+		(bool success, ) = msg.sender.call.value(amountToWithdraw)("");
+		require(success);
+	}
+}
+
+// safe secure method
+contract safeSecureMethod {
+	mapping(address => uint) private userBalances;
+	bool internal locked;
+
+	modifier noReentrant() {
+		require(!locked, "no re entrancy");
+		locked = true;
+		_;
+		locked = false;
+	}
+
+	function withdraw() public noReentrant {
+		uint amountToWithdraw = userBalances[msg.sender];
+		userBalances[msg.sender] = 0;
+		(bool success, ) = msg.sender.call.value(amountToWithdraw)("");
+		require(success);
+	}
+
+}
