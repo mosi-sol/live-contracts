a4fb4ec5b007d055e5716c19cb77e83d4d6dca3c
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-02-18 02:20:23 +0330

    Create StakeNftForReward.sol

---
+++ episode-6/StakeNftForReward.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract StakingRewards {
+    IERC20 public rewardsToken;
+    IERC721 public stakingToken;
+
+    uint public rewardRate = 100;
+    uint public lastUpdateTime;
+    uint public rewardPerTokenStored;
+
+    mapping(address => uint) public userRewardPerTokenPaid;
+    mapping(address => uint) public rewards;
+
+    uint private _totalSupply;
+    mapping(address => uint) private _balances;
+
+    constructor(address _stakingToken, address _rewardsToken) {
+        stakingToken = IERC721(_stakingToken);
+        rewardsToken = IERC20(_rewardsToken);
+    }
+
+    function rewardPerToken() public view returns (uint) {
+        if (_totalSupply == 0) {
+            return 0;
+        }
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _totalSupply);
+    }
+
+    function earned(address account) public view returns (uint) {
+        return
+            ((_balances[account] *
+                (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) +
+            rewards[account];
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        _;
+    }
+
+    function stake(uint _tokenId) external updateReward(msg.sender) {
+        stakingToken.transferFrom(msg.sender, address(this), _tokenId);
+    }
+
+    function withdraw(uint _tokenId) external updateReward(msg.sender) {
+        stakingToken.transferFrom(address(this), msg.sender, _tokenId);
+    }
+
+    function getReward() external updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        rewardsToken.transfer(msg.sender, reward);
+    }
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+// eip721 page
+interface IERC721{ /* is ERC165 */ 
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    
+    function balanceOf(address _owner) external view returns (uint256);
+    function ownerOf(uint256 _tokenId) external view returns (address);
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external payable;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function approve(address _approved, uint256 _tokenId) external payable;
+    function setApprovalForAll(address _operator, bool _approved) external;
+    function getApproved(uint256 _tokenId) external view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+}
+
+// eip165
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceID) 
+    external view returns (bool);
+}
+
+// for tracking
+interface IERC721TokenReceiver {
+    function onERC721Received(
+        address _operator, 
+        address _from, 
+        uint256 _tokenId, 
+        bytes calldata _data) 
+        external returns(bytes4);
+}
+
+// assembel metadata offchain/onchain
+interface IERC721Metadata { /* is ERC721 */
+    function name() external view returns (string calldata _name);
+    function symbol() external view returns (string calldata _symbol);
+    function tokenURI(uint256 _tokenId) external view returns (string calldata);
+}
+
+// iterte
+interface IERC721Enumerable { /* is ERC721 */
+    function totalSupply() external view returns (uint256);
+    function tokenByIndex(uint256 _index) external view returns (uint256);
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
+}
