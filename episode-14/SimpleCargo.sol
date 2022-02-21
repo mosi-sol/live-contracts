49cde212018de63ab9a5ccb7bb5159ab4cf46cf5
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-02-22 01:00:05 +0330

    Create SimpleCargo.sol

---
+++ episode-14/SimpleCargo.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8;
+
+contract Cargo {
+	mapping(uint => Thing) public things; // id -> things
+
+	struct Thing {
+		string name;
+		string position;
+	}
+
+	uint index = 1;
+
+	function addThings(string memory _name, string memory _pos) public {
+		things[index] = Thing(_name, _pos);
+		index++;
+	}
+
+	function updateThing(uint _id, string memory _name, string memory _pos) public {
+		things[_id] = Thing(_name, _pos);
+	}
+
+	// delete should check before teach 
+	function soldThing(uint _index) public {
+		delete things[_index];
+	}
+
+}
