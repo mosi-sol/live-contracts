e4d58ca15115298ecc7bc958f627b85677045968
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-04-02 23:53:33 +0430

    Update AbstractAW.sol

--- episode-20/AbstractAW.sol
+++ episode-20/AbstractAW.sol
@@ -5,6 +5,20 @@ pragma solidity 0.8;
 // version 1
 
 abstract contract AWithdrawPassword{
+    
+    bytes16 immutable hashedPass = 0xfb39401d483e3b8ed34e9d5cb7b345ab;
+    bool internal locked;
+
+	modifier noReentrant() {
+		require(!locked, "no re entrancy");
+		locked = true;
+		_;
+		locked = false;
+	}
+
+    event Deposit();
+
+    receive() external payable {}
 
     event Deposit();
 
